15d4f735f14dfc714abea23a0cc0ceaca9e2dca1
harmenkant <harmen.kant@edu-kw1c.nl>
2019-03-22 12:32:10 +0100

    Nav works a bit of a donate page update

---
+++ script.js
@@ -0,0 +1,16 @@
+function InfoPage()
+{
+    location = "index.html"
+}
+function NewsPage()
+{
+    location = "nieuws.html"
+}
+function ContactPage()
+{
+    location = "contact.html"
+}
+function DonatePage()
+{
+    location = "donate.html"
+}
